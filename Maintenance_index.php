172bfb57a0abd54cfc8c58d39b35395ae0fa1f02
Edd <55090875+Corey-Stowe@users.noreply.github.com>
2022-11-11 02:12:04 +0700

    Add check Algorithm command

--- Maintenance_index.php
+++ Maintenance_index.php
@@ -35,12 +35,12 @@ function GetStr($string, $start, $end){
 
 if (strpos($message, "/start") === 0){
  sendMessage($chatId, "<b>Hello @$usernamee!!%0AType /help to know all my commands!!%0A%0ABot Made by: Stowe .</b>", $message_id);
-sendMessage($chatId, "<b><u> Bot Maintenance mode is enabled !.</u></b>%0A%0A <b> The are 4 commands available for you use. Please use <code>/help</code> to show available commands</b>", $message_id);
+sendMessage($chatId, "<b><u> Bot Maintenance mode is enabled !.</u></b>%0A%0A <b> The are 5 commands available for you use. Please use <code>/help</code> to show available commands</b>", $message_id);
 }
 
 //////////=========[help Command]=========//////////
  elseif (strpos($message, "/help") === 0){
-sendMessage($chatId, "<b><u> LIST OF COMMANDS AVAILABE</u></b>%0A%0A%0A<b>check bin:<code>/bin xxxxx</code></b>%0A<b>Check sk: <code>/sk sk_livexxxxxxxxxxx</code></b>%0A <b>Your info: <code>/info</code></b>%0<b>Change Log version: <code>/log</code></b> %0A%0A<b><code>Maintenance mode is enabled</code></b>%0A<b>Bot version:</b> <code>2.0 - DEBUG</code> ", $message_id);
+sendMessage($chatId, "<b><u> LIST OF COMMANDS AVAILABE</u></b>%0A%0A%0A<b>check bin:<code>/bin xxxxx</code></b>%0A<b>Check sk: <code>/sk sk_livexxxxxxxxxxx</code></b>%0A<b>check Algorithm card:<code>/agt xxxxxxxxxx|xx|xx|xxx </code></b>%0A<b>Your info: <code>/info</code></b>%0A<b>Change Log version: <code>/log</code></b> %0A%0A<b><code>Maintenance mode is enabled</code></b>%0A<b>Bot version:</b> <code>2.0 - DEBUG</code> ", $message_id);
 }
 //////////=========[Info Command]=========//////////
 
@@ -128,7 +128,7 @@ elseif (strpos($message, "/aut") === 0){
  $mm = multiexplode(array(":", "/", " ", "|"), $message)[2];
  $yy = multiexplode(array(":", "/", " ", "|"), $message)[3];
  $cvv = multiexplode(array(":", "/", " ", "|"), $message)[4];
- sendMessage($chatId, "<b><u> Bot Maintenance mode is enabled !.</u></b>%0A%0A <b> The are 4 commands available for you use. Please use <code>/help</code> to show available commands</b>%0A%0A<b><code>Maintenance mode is enabled</code></b>%0A<b>Bot version:</b> <code>2.0 - DEBUG</code>", $message_id);
+ sendMessage($chatId, "<b><u> Bot Maintenance mode is enabled !.</u></b>%0A%0A <b> The are 5 commands available for you use. Please use <code>/help</code> to show available commands</b>%0A%0A<b><code>Maintenance mode is enabled</code></b>%0A<b>Bot version:</b> <code>2.0 - DEBUG</code>", $message_id);
 }
 //=====================[chg]=============================//
 elseif (strpos($message, "/chg") === 0){
@@ -138,7 +138,7 @@ elseif (strpos($message, "/chg") === 0){
     $mm = multiexplode(array(":", "/", " ", "|"), $message)[2];
     $yy = multiexplode(array(":", "/", " ", "|"), $message)[3];
     $cvv = multiexplode(array(":", "/", " ", "|"), $message)[4];
-    sendMessage($chatId, "<b><u> Bot Maintenance mode is enabled !.</u></b>%0A%0A <b> The are 4 commands available for you use. Please use <code>/help</code> to show available commands</b>%0A%0A<b><code>Maintenance mode is enabled</code></b>%0A<b>Bot version:</b> <code>2.0 - DEBUG</code>", $message_id);
+    sendMessage($chatId, "<b><u> Bot Maintenance mode is enabled !.</u></b>%0A%0A <b> The are 5 commands available for you use. Please use <code>/help</code> to show available commands</b>%0A%0A<b><code>Maintenance mode is enabled</code></b>%0A<b>Bot version:</b> <code>2.0 - DEBUG</code>", $message_id);
    }
 //=====================[rape]=============================//
 elseif (strpos($message, "/rape") === 0){
@@ -159,7 +159,7 @@ elseif (strpos($message, "/kcc") === 0){
     $mm = multiexplode(array(":", "/", " ", "|"), $message)[2];
     $yy = multiexplode(array(":", "/", " ", "|"), $message)[3];
     $cvv = multiexplode(array(":", "/", " ", "|"), $message)[4];
-    sendMessage($chatId, "<b><u> Bot Maintenance mode is enabled !.</u></b>%0A%0A <b> The are 4 commands available for you use. Please use <code>/help</code> to show available commands</b>%0A%0A<b><code>Maintenance mode is enabled</code></b>%0A<b>Bot version:</b> <code>2.0 - DEBUG</code>", $message_id);
+    sendMessage($chatId, "<b><u> Bot Maintenance mode is enabled !.</u></b>%0A%0A <b> The are 5 commands available for you use. Please use <code>/help</code> to show available commands</b>%0A%0A<b><code>Maintenance mode is enabled</code></b>%0A<b>Bot version:</b> <code>2.0 - DEBUG</code>", $message_id);
    }
 //=====================[ccn]=============================//
 elseif (strpos($message, "/ccn") === 0){
@@ -169,8 +169,86 @@ elseif (strpos($message, "/ccn") === 0){
     $mm = multiexplode(array(":", "/", " ", "|"), $message)[2];
     $yy = multiexplode(array(":", "/", " ", "|"), $message)[3];
     $cvv = multiexplode(array(":", "/", " ", "|"), $message)[4];
-    sendMessage($chatId, "<b><u> Bot Maintenance mode is enabled !.</u></b>%0A%0A <b> The are 4 commands available for you use. Please use <code>/help</code> to show available commands</b>%0A%0A<b><code>Maintenance mode is enabled</code></b>%0A<b>Bot version:</b> <code>2.0 - DEBUG</code>", $message_id);
+    sendMessage($chatId, "<b><u> Bot Maintenance mode is enabled !.</u></b>%0A%0A <b> The are 5 commands available for you use. Please use <code>/help</code> to show available commands</b>%0A%0A<b><code>Maintenance mode is enabled</code></b>%0A<b>Bot version:</b> <code>2.0 - DEBUG</code>", $message_id);
    }
+//=====================[agt]=============================//
+elseif (strpos($message, "/agt") === 0){
+    $skeys = array(
+        1 => 'pk_live_7eJoMVqqEZ85eX2E82EZy9yF001tORzSsV', // Enter at least one sk key
+      //2 => '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',-----------------|
+      //3 => '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',                 | Uncomment this, if you want to add more sk keys :)
+      //4 => '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',                 |
+      //5 => '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',-----------------|
+      ); 
+      $skey = array_rand($skeys);
+      $sk = $skeys[$skey];
+$message = substr($message, 4);
+$amt = multiexplode(array("/", ":", " ", "|"), $message)[0];
+$cc = multiexplode(array(":", "/", " ", "|"), $message)[1];
+$mm = multiexplode(array(":", "/", " ", "|"), $message)[2];
+$yy = multiexplode(array(":", "/", " ", "|"), $message)[3];
+$cvv = multiexplode(array(":", "/", " ", "|"), $message)[4];
+if (empty($amt)) {
+$amt = '1';
+}
+        $amount = $amt * 100;
+        $lista = ''.$cc.'|'.$mm.'|'.$yy.'|'.$cvv.'';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://lookup.binlist.net/'.$cc.'');
+        curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Host: lookup.binlist.net',
+        'Cookie: _ga=GA1.2.549903363.1545240628; _gid=GA1.2.82939664.1545240628',
+        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'));
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, '');
+        $time = $info['total_time'];
+        $httpCode = $info['http_code'];
+        $time = substr($time, 0, 4);
+        $fim = curl_exec($ch);
+        $bank = GetStr($fim, '"bank":{"name":"', '"');
+        $name = GetStr($fim, '"name":"', '"');
+        $brand = GetStr($fim, '"brand":"', '"');
+        $country = GetStr($fim, '"country":{"name":"', '"');
+        $emoji = GetStr($fim, '"emoji":"', '"');
+        $scheme = GetStr($fim, '"scheme":"', '"');
+        $type = GetStr($fim, '"type":"', '"');
+        if(strpos($fim, '"type":"credit"') !== false){
+        $bin = 'Credit';
+        }else{
+        $bin = 'Debit';
+        };
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://api.stripe.com/v1/payment_methods');
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_USERPWD, $sk. ':' . '');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=card&card[number]='.$cc.'&card[cvc]='.$cvv.'&card[exp_month]='.$mm.'&card[exp_year]='.$yy.'&billing_details[name]=Corey Stowe&billing_details[email]=cstowe083@gmail.com&billing_details[address][country]=US&billing_details[address][line1]=2509 Mountainview Dr&billing_details[address][city]=Corint&billing_details[address][postal_code]=87210&billing_details[address][state]=Texas&payment_user_agent=stripe.js%2Fc8e99151a%3B+stripe-js-v3%2Fc8e99151a%3B+checkout');
+        $result1 = curl_exec($ch);
+        $tok1 = Getstr($result1,'"id": "','"');
+        $msg1 = Getstr($result1,'"message": "','"');
+        $msg1a = Getstr($result1,'"decline_code": "','"');
+        if(strpos($result1, "payment_method" )) {
+          sendMessage($chatId, "%0A<b>Card: <code>$lista</code></b>%0A<b>Status -» APPROVED ✅</b>%0A<b>Response -» Algorithm Card is vaild </b>%0A<b>Gateway -» Stripe AGT v1 </b>%0A%0A-----------[Bin details]--------------%0A%0ABANK NAME: $bank %0ANAME : $name %0ABRAND: $brand %0ASCHEME: $scheme %0ATYPE: $type %0ACOUNTRY:$country%0A%0A-----------------------------%0A%0A<b>⋆ Checked By: @$usernamee</b>%0A<b>⋆ Bot By:@stowe_245</b>%0A%0A<b><code>Maintenance mode is enabled</code></b>%0A<b>Bot version:</b> <code>2.0 - DEBUG</code>", $message_id);
+           return;
+        }
+        if (strpos($result1, "card_declined" )) {
+            sendMessage($chatId, "%0A<b>Card: <code>$lista</code></b>%0A<b>Status -» DECLINED ❌</b>%0A<b>Response -» Check Algorithm card is falied </b>%0A <b>Respone 2 -» : $msg1a</b>%0A<b>Gateway -» Stripe AGT v1 </b>%0A%0A-----------[Bin details]--------------%0A%0ABANK NAME: $bank %0ANAME : $name %0ABRAND: $brand %0ASCHEME: $scheme %0ATYPE: $type %0ACOUNTRY:$country%0A%0A-----------------------------%0A%0A<b>⋆ Checked By: @$usernamee</b>%0A<b>⋆ Bot By:@stowe_245</b>%0A%0A<b><code>Maintenance mode is enabled</code></b>%0A<b>Bot version:</b> <code>2.0 - DEBUG</code>", $message_id);
+            return;
+        }
+        if (strpos($result1, "card_error" )) {
+            sendMessage($chatId, "%0A<b>Card: <code>$lista</code></b>%0A<b>Status -» DECLINED ❌</b>%0A<b>Response -» Check Algorithm card is falied </b>%0A <b>Respone 2 -» : $msg1a</b>%0A<b>Gateway -» Stripe AGT v1 </b>%0A%0A-----------[Bin details]--------------%0A%0ABANK NAME: $bank %0ANAME : $name %0ABRAND: $brand %0ASCHEME: $scheme %0ATYPE: $type %0ACOUNTRY:$country%0A%0A-----------------------------%0A%0A<b>⋆ Checked By: @$usernamee</b>%0A<b>⋆ Bot By:@stowe_245</b>%0A%0A<b><code>Maintenance mode is enabled</code></b>%0A<b>Bot version:</b> <code>2.0 - DEBUG</code>", $message_id);
+            return;
+
+        }
+        else {
+            sendMessage($chatId, "<b>SERVER ERROR</b>%0A<b>Response -» <code>$msg1 | $msg1a | $result1 </code></b>%0A%0A<b><code>Maintenance mode is enabled</code></b>%0A<b>Bot version:</b> <code>2.0 - DEBUG</code>", $message_id);
+        }
+    
+    }
 
 function sendMessage ($chatId, $message, $message_id){
     $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".$message."&reply_to_message_id=".$message_id."&parse_mode=HTML";
