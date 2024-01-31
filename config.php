43ecb2f15c25767cd2f9cf542625d14e9df3ecc0
Edd <55090875+Corey-Stowe@users.noreply.github.com>
2024-01-31 22:23:08 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Stowe bot Setup V3.3
 
 //This for telegram bot token which you can get from @BotFather
-$botToken = "5020014857:AAEKpsdaG879QntGTTXbK3qgbc5c6nxB46k";
+$botToken = "";
 
 //this for test case from stripe
 $sktest = "sk_test_51NGgGlEzbVZNtSWJH3a3FHiAhAFOa3RpzG64C1MOIQM7bdf2bXB8VuPLo40CYbQzHtrJVZEbWRSDBOMH9TaO5jOr00jE5UZPlv";
@@ -10,8 +10,8 @@ $pktest = "pk_test_51NGgGlEzbVZNtSWJdiHPiNa0lZJP5BHCULRrLTqy0ly69QCGO4Y0Zd0ncXDg
 
 //For live card payment
 // $sk = "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";
- $pk = "pk_live_51O1nfdH6uF69TOEF6GKYoGL3gUFpwhc15zmWAPkpy6gleaz9YxCM4I2p3yZTWnLN9baiJamgCiKlXb1pM5ixBnnH0014FckPCR";
- $sk = "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";
+ $pk = "";
+ $sk = "";
 // $pk = "pk_live_51NqwDiAC5dHsa7gzW72J83ISb8IBkYOlvrrmVXgwKYCfa6UNGHjKJGZjsG1T0M5EVG4SYMz6tqKHvyptRUCWt44L00oRHspcFO";
 
 //ADMIN ID config
@@ -20,5 +20,4 @@ $admin_id = "1090979938"; //For recieving bug or error reports
 
 // Recieving bug or error reports
 $buger = true; //true or false for enable or disable bug report
-$pmsend = true; //true or false for enable or disable card log send to pm
 ?>
